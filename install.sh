9aca8957f9376fdc78b29a7ad789a360a338937c
河本剛志 <kawamoto@digitalize.co.jp>
2014-06-26 15:26:39 +0900

    pecoの設定を追加

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ DIR_SCRIPT="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
 cd ${HOME}
 
 # pecoを実行可能に設定
+ln -sf ${DIR_SCRIPT}/dotfiles/peco ${HOME}/.peco
 ln -sf ${DIR_SCRIPT}/dotfiles/snippets ${HOME}/.snippets
 
 # install xcode command line tools
